0a7a91ad6b5d71859e03a2f74c84c377d2ae3950
Umarbek Orifov <umarbek.orifov@gmail.com>
2020-12-04 18:47:07 +0500

    author and status added

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime 
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Task(models.Model):
@@ -7,17 +8,17 @@ class Task(models.Model):
     completed = models.BooleanField(default=False, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
-
-    # status will be set to expired 
-    # if deadline has passed, otherwise it is still open
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     @property
     def status(self):     
       now = timezone.now()
       if now < self.deadline:
-        return 'open'
+        return 'open' 
       else:
         return 'expired'
       
     def __str__(self):
       return self.title
+
+
